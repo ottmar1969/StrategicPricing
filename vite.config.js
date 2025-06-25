69fc0708619caaf4c70455d97811c09e977e31e9
ottmar1969 <158744549+ottmar1969@users.noreply.github.com>
2025-06-25 20:07:28 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react( )],
+  root: ".",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    target: 'es2015',
+    rollupOptions: {
+      input: 'src/main.jsx',
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: false,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: false,
+  },
+  base: '/',
+  publicDir: 'public',
+})
