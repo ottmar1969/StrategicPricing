1f07853bdf56efce74a82b17c39d48275239f3ae
ottmar1969 <158744549+ottmar1969@users.noreply.github.com>
2025-06-25 22:54:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 export default defineConfig({
-  plugins: [react( )],
-  root: ".",
+  plugins: [react()],
+  root: './client',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
     target: 'es2015',
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-        index: 'index.html' // This line explicitly includes index.html in the build
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
+      '@shared': path.resolve(__dirname, './shared'),
     },
   },
   server: {
     host: '0.0.0.0',
     port: 3000,
     strictPort: false,
+    hmr: {
+      clientPort: 443 // Use 443 for HTTPS
+    },
+    allowedHosts: ['*'] // Allow all hosts for development
   },
   preview: {
     host: '0.0.0.0',
